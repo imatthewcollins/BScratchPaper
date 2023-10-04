b815c73573fab651315bd2a34b374af706989ec5
Matthew Collins <m.collins@etoncollege.org.uk>
2023-10-04 09:58:22 +0100

    Initial Commit

---
+++ BComX-1 Example Project/BComX_1_Example_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BComX_1_Example_ProjectApp.swift
+//  BComX-1 Example Project
+//
+//  Created by Collins, Matthew - MC on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BComX_1_Example_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
